f04f9bc0fbae5e8a298781c9d374fffeb780eabf
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-06-02 00:31:45 +0630

    update hardhat config file and add deploy

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/AmV8stROsMN7odkLWqpIOY8zffaq9hqo",
+      accounts: [
+        "d4c49a86fee3001caf4821399c425699a5a2e8597000c6646d77e42e2b3b73ae",
+      ],
+    },
+  },
 };
