16102b5d50b4b7cb50ae0038cb55e5d8660d493c
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-06-01 12:44:04 +0630

    update welcome layout

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -19,12 +19,12 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 
 const Welcome = () => {
   const [isLoading, setIsLoading] = useState(false);
-  
+
   const connectWallet = () => {};
   const handleSubmit = () => {};
   return (
     <div className="w-full flex justify-center items-center">
-      <div className="flex flex-col items-start justify-between md:p-20 py-12 px-4">
+      <div className="flex flex-col lg:flex-row items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start flex-col md:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
